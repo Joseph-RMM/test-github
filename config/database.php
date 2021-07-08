0a5660680b5ccaebffad8d0d549df985225cc9f3
Joseph-RMM <72950481+Joseph-RMM@users.noreply.github.com>
2021-07-07 21:44:01 -0500

    test webhost

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+class Conectar{
+    public static function conexion(){
+        $conexion = new mysqli("localhost","id17208486_movilidadutp",'n}q7){~I?E%Wh9$F',"id17208486_movilidadutpinfo");
+        return $conexion;
+    }
+}
+
+?>
---
+++ view/principal.php
@@ -0,0 +1,29 @@
+<?php?>
+
+<!DOCTYPE html>
+<head>
+    <title>Movilidad UTP</title>
+</head>
+<body>
+    <h1>Aquí va la vista</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Descripción Corta</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php  foreach($data["Convocatorias"] as $dato){
+                echo "<tr>";
+                echo "<td>".$dato["Nombre"]."</td>";
+                echo "<td>".$dato["DescripcionCorta"]."</td>";
+                echo "</tr>";
+
+            } ?>
+        </tbody>
+        
+    </table>
+</body>
+</html>
