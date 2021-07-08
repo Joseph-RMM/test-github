7f4b3c6adadc66c2b3bad2dbf8a01e1a81ae7e0b
Joseph-RMM <72950481+Joseph-RMM@users.noreply.github.com>
2021-07-07 22:02:55 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 class Conectar{
     public static function conexion(){
-        $conexion = new mysqli("localhost","id17208486_movilidadutp",'n}q7){~I?E%Wh9$F',"id17208486_movilidadutpinfo");
+        $conexion = new mysqli("https://movilidadutp.000webhostapp.com/","id17208486_movilidadutp",'n}q7){~I?E%Wh9$F',"id17208486_movilidadutpinfo");
         return $conexion;
     }
 }
