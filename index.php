fc4358da129f8512616918dbc4cd414e5ef17d2f
Joseph-RMM <72950481+Joseph-RMM@users.noreply.github.com>
2021-07-07 22:29:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 
 <?php
 //Incluyo los archivos necesarios
-require_once "./config/database.php";
-require_once "./model/model.php";
-require_once "./controller/controller.php";
+require_once "config/database.php";
+require_once "model/model.php";
+require_once "controller/controller.php";
 
 $control = new WebController();
 
